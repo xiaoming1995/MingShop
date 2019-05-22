0e3f2b32c65799a72213f44a338928d9a650b054
vagrant <vagrant@homestead>
2019-05-22 17:39:12 +0000

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -7,7 +7,8 @@ use App\Models\UserAddress;
 use App\Http\Requests\UserAddressRequest;
 
 class UserAddressesController extends Controller
-{
+{      
+
     public function index(Request $request)
     {
         return view('user_addresses.index', [
@@ -15,11 +16,20 @@ class UserAddressesController extends Controller
         ]);
     }
 
+    //添加页面
     public function create()
     {
     	return view('user_addresses.create_and_edit',['address'=> new UserAddress()]);
     }
 
+    //修改页面
+    public function edit(UserAddress $user_address)
+    {   
+        $this->authorize('own', $user_address);
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+    
+    //存储方法
     public function store(UserAddressRequest $request)
     {
         $request->user()->addresses()->create($request->only([
@@ -34,5 +44,34 @@ class UserAddressesController extends Controller
 
         return redirect()->route('user_addresses.index');
     }
+
+
+    //修改方法
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {   
+        $this->authorize('own', $user_address);
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    //删除方法
+    public function destroy(UserAddress $user_address)
+    {   
+        $this->authorize('own', $user_address);
+        $user_address->delete();
+
+        return [];
+    }
+
+
 
 }
--- resources/views/user_addresses/index.blade.php
+++ resources/views/user_addresses/index.blade.php
@@ -25,8 +25,9 @@
                 <td>{{ $address->zip }}</td>
                 <td>{{ $address->contact_phone }}</td>
                 <td>
-                  <button class="btn btn-primary">修改</button>
-                  <button class="btn btn-danger">删除</button>
+                  <a href="{{route('user_addresses.edit',['user_addresses'=>$address->id])}}" class="btn btn-primary">修改</a>
+                  <!-- 把之前删除按钮的表单替换成这个按钮，data-id 属性保存了这个地址的 id，在 js 里会用到 -->
+                  <button class="btn btn-danger btn-del-address" type="submit" data-id="{{ $address->id }}">删除</button>
                 </td>
               </tr>
             @endforeach
@@ -36,4 +37,48 @@
       </div>
     </div>
   </div>
+@endsection
+@section('scriptsAfterJs')
+<script>
+$(document).ready(function() {
+  // 删除按钮点击事件
+  $('.btn-del-address').click(function() {
+    // 获取按钮上 data-id 属性的值，也就是地址 ID
+    var id = $(this).data('id');
+    // 调用 sweetalert
+    swal({
+        title: "确认要删除该地址？",
+        icon: "warning",
+        buttons: ['取消', '确定'],
+        dangerMode: true,
+      })
+    .then(function(willDelete) { // 用户点击按钮后会触发这个回调函数
+      // 用户点击确定 willDelete 值为 true， 否则为 false
+      if(willDelete){
+        // 调用删除接口，用 id 来拼接出请求的 url
+        axios.delete('/user_addresses/' + id).then(function (res) {
+          if(res.status == 200) {
+            // 请求成功之后重新加载页面
+            swal("删除成功!","您已经永久删除了这条数据。","success").then( res =>{
+                 if(res){
+                    location.reload();
+                 }
+            })
+
+          }else {
+
+             swal({title:"删除失败！",text:"操作失败请检查当前操作！",type:"error"});
+          }
+          
+        })
+      }else{
+        // 用户点了取消，啥也不做
+        return;
+      }
+        
+    });
+    
+  });
+});
+</script>
 @endsection
