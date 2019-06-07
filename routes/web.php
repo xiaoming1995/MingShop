696c03ba2c741cde2ce41c4cd0db01f7e063287f
vagrant <vagrant@homestead>
2019-06-07 16:34:25 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::redirect('/', '/products')->name('root');   //商品列表
 Route::get('products', 'ProductsController@index')->name('products.index'); 
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 Auth::routes(['verify'=>true]);
 
@@ -27,5 +27,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
 
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
