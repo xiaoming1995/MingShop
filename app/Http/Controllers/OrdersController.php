c6f64180d5a99532006c93edbaf1c4a5528d76bc
vagrant <vagrant@homestead>
2019-06-27 16:03:25 +0000

    用户界面 - 订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Http\Requests\OrderRequest;
 use App\Exceptions\InvalidRequestException;
 use App\Models\ProductSku;
@@ -12,6 +13,19 @@ use App\Jobs\CloseOrder;
 
 class OrdersController extends Controller
 {	
+
+	public function index(Request $request)
+	{
+		$orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku']) 
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
+	}
+
 
 	/*
 		代码解析：
@@ -78,7 +92,7 @@ class OrdersController extends Controller
         });
 
         $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
-        
+
         return $order;
     }
 }
