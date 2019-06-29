dc334399df43a33e64be366b4dca43c60077fc8b
vagrant <vagrant@homestead>
2019-06-29 18:41:19 +0000

    微信支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,6 +33,7 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
+            $config['notify_url'] = 'http://requestbin.fullcontact.com/qvhvy5qv';
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {
