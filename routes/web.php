397c7965c0f6ac2efbac6c0528ae6935501f7f02
vagrant <vagrant@homestead>
2019-07-01 07:45:44 +0000

    确认收货

--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
-    Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');   //微信
+    Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');   //微信订单处理
+    Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');   //收获确定
 });
-Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');  //
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::get('alipay', function() {
